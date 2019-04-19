e3a42d79aab34ae5813e59a9900b69cc887b6c04
Michael Hoffman <michaelhoffman@Michaels-MacBook-Air.local>
2019-04-19 14:58:47 -0600

    Scorecard Layout

--- control.js
+++ control.js
@@ -13,21 +13,21 @@ class Player{
             let playerbox = $(".playerbox");
             scorecard.append(`<div class="nameLine">${this.name}</div>`)
             scorecard.append(`<div class="encapsulate">
-            <div class="spacing">Yards</div>
-            <div class="spacing2">Par</div>
+            <div class="spacing3">Yards</div>
+            <div class="spacing3">Par</div>
             <div class="spacing3">Handycap </div>
-            <div class="spacing4" >Score </div>
-            <div class="spacing5">Total </div>
+            <div class="spacing3" >Score </div>
+            <div class="spacing3">Total </div>
 
             </div>` )
         for(let i in holes){
             let hole = holes[i].teeBoxes[this.tee];
             scorecard.append(`<div class="encapsulate">
-            <div class="spacing">${hole.yards}</div>
+            <div class="spacing4">${hole.yards}</div>
             <div class="spacing2">${hole.par}</div>
-            <div class="spacing3">${hole.hcp}</div>
-            <input class="spacing4"></input>
-            <div class="spacing5 scoreAdd" >0</div>
+            <div class="spacing">${hole.hcp}</div>
+            <input class="spacing2"></input>
+            <div class="spacing scoreAdd" >0</div>
             </div>`)
         }
         $("input").change(function(e) {
